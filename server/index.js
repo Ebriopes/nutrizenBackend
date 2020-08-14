dbc11f44b162dd1eacfbc9ebd1a5adbaef0802ae
El sucio Dann <elsuciodann@MacBook-Air-de-Mefistofeles.local>
2020-08-14 18:14:17 -0500

    add cors

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 const router = require("../routes");
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: '/tmp/'
